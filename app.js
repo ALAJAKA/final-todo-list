d2f5bf3490884c46877749223bce177ca00f628d
ALAJAKA <tkvl1017@gmail.com>
2023-03-02 19:44:04 +0900

    layout add / main page, side nav, signUp page

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('static'));
+app.use(express.static('./src/static'));
 
 app.use('/', router);
 
