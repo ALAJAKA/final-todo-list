7e1b39269bf3c15b06be6555ca32b5febef25359
ALAJAKA <tkvl1017@gmail.com>
2023-03-08 12:39:18 +0900

    create : pageRoute

---
+++ src/routes/pageRoute.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+//todolist월별
+router.get('/page/todoListMonth', async (req,res) => {
+    return res.render('todolist');
+});
+router.get('/page/bucketList', async (req,res) => {
+    return res.render('bucketList');
+});
+router.get('/page/todoListDetail', async (req,res) => {
+    return res.render('todolistDetail');
+});
+// router.get('/', async (req,res) => {
+//     return res.render('main');
+// });
+
+router.get('/', async (req,res) => {
+    return res.render('main');
+});
+
+module.exports = router;
