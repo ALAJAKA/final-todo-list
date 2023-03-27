a762dbdc03211ffa34bc90ed1ad1e133d2da1471
ALAJAKA <tkvl1017@gmail.com>
2023-03-28 00:52:45 +0900

    fixed bucketListController try / catch 수정 fixed main 페이지 달력 타임존 수정

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,9 +5,14 @@ class UserController {
 
   //회원가입
   login = async (req,res)=>{
-      const id_token = req.body.data.split('id_token=')[1].split('&')[0];
-      const access_token = await this.userService.login(id_token);
-      res.status(200).json(access_token);
+      try{
+          const id_token = req.body.data.split('id_token=')[1].split('&')[0];
+          const access_token = await this.userService.login(id_token);
+          res.status(200).json(access_token);
+      }catch(e){
+          console.log(e);
+      }
+
   }
 
 }// class 끝
